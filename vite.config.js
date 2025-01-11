e8ea17e934c21589895bebdcd25fc26c94ce1ff4
chenbz <97715531+chenbz777@users.noreply.github.com>
2025-01-11 16:24:28 +0800

    feat: init

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import viteCompression from 'vite-plugin-compression';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+
+  return {
+    plugins: [
+      vue(),
+      viteCompression({
+        verbose: false  // 是否在控制台输出压缩结果
+        // deleteOriginFile: true  // 压缩后是否删除源文件
+      }),
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()]
+      })
+    ],
+    server: { // 开发服务器配置
+    },
+    base: '/',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    },
+    css: {
+    }
+  };
+});
