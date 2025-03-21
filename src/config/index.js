ae12a87456150846f9755bc84f23d589bfbec0c2
chenbz <97715531+chenbz777@users.noreply.github.com>
2025-03-21 11:27:25 +0800

    style: 添加配置注释

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,14 @@ import configProd from './config.prod';
 import configDev from './config.dev';
 
 
-const importModules = import.meta.glob('./*.js', { eager: true });
+/**
+ * 配置合并逻辑
+ * 1. 取默认配置
+ * 2. 根据环境变量 NODE_ENV 区分环境(开发/生产)
+ * 3. 根据当前域名区分环境
+ * 
+ * 优先级：域名配置 > 生产配置 > 开发配置 > 默认配置
+ */
 
 const config = configDefault;
 
@@ -14,6 +21,8 @@ if (process.env.NODE_ENV === 'development') {
 if (process.env.NODE_ENV === 'production') {
   Object.assign(config, configProd);
 }
+
+const importModules = import.meta.glob('./*.js', { eager: true });
 
 for (const path in importModules) {
   const module = importModules[path];
