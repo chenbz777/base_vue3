3f471c16eab93b2c2817d5de2a82610041843638
chenbz <97715531+chenbz777@users.noreply.github.com>
2025-03-26 11:16:27 +0800

    perf: 优化打包体积

--- src/main.js
+++ src/main.js
@@ -3,16 +3,11 @@ import App from './App.vue';
 import router from './router';  // 路由
 import 'animate.css';  // 引入animate动画样式
 import 'element-plus/dist/index.css';
-import ElementPlus from 'element-plus';
-import zhCn from 'element-plus/dist/locale/zh-cn.mjs';
 
 
 const app = createApp(App);
 
 app.use(router);
-app.use(ElementPlus, {
-  locale: zhCn
-});
 
 // 自动导入并注册 `components` 文件夹下的所有 `.vue` 文件
 const components = import.meta.glob('@/components/*.vue', { eager: true });
